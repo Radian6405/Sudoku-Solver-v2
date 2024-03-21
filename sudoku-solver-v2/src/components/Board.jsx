9707481c4bc40bcf9b7bfcdaf375d46028c997d1
Sai Vardhan <vardhan.6405@gmail.com>
2024-03-21 12:43:58 +0530

    finished styling body (excluding board)

---
+++ sudoku-solver-v2/src/components/Board.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Board() {
+  return (
+    <>
+      <div className="bg-black w-[538px]  h-[538px]  "></div>;
+    </>
+  );
+}
+
+export default Board;
--- sudoku-solver-v2/src/components/Body.jsx
+++ sudoku-solver-v2/src/components/Body.jsx
@@ -1,11 +1,13 @@
 import React, { Children } from "react";
 import { useSearchParams } from "react-router-dom";
+import Unlock from "./icons/unlock";
+import Board from "./Board";
 
 function Body() {
   const [searchParams, setSearchParams] = useSearchParams();
   return (
-    <div className="px-32 py-16 bg-background flex justify-center">
-      <div className="h-28 flex flex-row gap-32">
+    <div className="px-28 py-10 bg-background flex flex-col ">
+      <div className="h-28 flex flex-row items-center justify-center gap-32 ">
         <div
           className="w-40 h-16
             flex justify-center items-center 
@@ -13,25 +15,49 @@ function Body() {
         >
           Difficulty:
         </div>
-        <div className="h-28 flex flex-row gap-10">
-          <Button OnClick={() => setSearchParams({ difficulty: "easy" })}>
+        <div className="h-28 flex flex-row items-center gap-10">
+          <DiffButton OnClick={() => setSearchParams({ difficulty: "easy" })}>
             Easy
-          </Button>
-          <Button OnClick={() => setSearchParams({ difficulty: "medium" })}>
+          </DiffButton>
+          <DiffButton OnClick={() => setSearchParams({ difficulty: "medium" })}>
             Medium
-          </Button>
-          <Button OnClick={() => setSearchParams({ difficulty: "hard" })}>
+          </DiffButton>
+          <DiffButton OnClick={() => setSearchParams({ difficulty: "hard" })}>
             Hard
-          </Button>
+          </DiffButton>
         </div>
+      </div>
 
-        {/*<div className="text-white">{searchParams.get("difficulty")}</div>*/}
+      <div className=" flex justify-center p-5 gap-10">
+        <Board />
+        <div className=" w-[304px]  h-[538px] py-10 flex flex-col items-center gap-[60px] ">
+          <div className=" h-[304px] grid grid-cols-3 gap-8 ">
+            <NumButton>1</NumButton>
+            <NumButton>2</NumButton>
+            <NumButton>3</NumButton>
+            <NumButton>4</NumButton>
+            <NumButton>5</NumButton>
+            <NumButton>6</NumButton>
+            <NumButton>7</NumButton>
+            <NumButton>8</NumButton>
+            <NumButton>9</NumButton>
+          </div>
+          <div
+            className="w-[200px] h-[80px] bg-secondary rounded-lg
+                flex justify-center items-center gap-3
+                font-inria text-4xl text-clip overflow-hidden text-text
+              hover:bg-accent hover:cursor-pointer"
+          >
+            <Unlock />
+            Solve
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-function Button({ OnClick, children }) {
+function DiffButton({ OnClick, children }) {
   return (
     <>
       <div
@@ -48,3 +74,23 @@ function Button({ OnClick, children }) {
 }
 
 export default Body;
+
+function NumButton({ children }) {
+  return (
+    <>
+      <div
+        className="w-[80px] h-[80px] rounded-[7px] bg-secondary
+                     flex justify-center items-center 
+                     font-inria text-5xl text-clip overflow-hidden text-text
+                     hover:bg-accent hover:cursor-pointer"
+      >
+        {" "}
+        {children}{" "}
+      </div>
+    </>
+  );
+}
+
+{
+  /*<div className="text-white">{searchParams.get("difficulty")}</div>*/
+}
