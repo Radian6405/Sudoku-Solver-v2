657e280d7ba3d60118ca888da1ca243b4f47ac01
Sai Vardhan <vardhan.6405@gmail.com>
2024-04-01 17:50:50 +0530

    added footer

--- sudoku-solver-v2/src/App.jsx
+++ sudoku-solver-v2/src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Body />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Body />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
   );
 };
---
+++ sudoku-solver-v2/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-background text-text h-24 flex border-t-2 border-accent"></div>
+  );
+}
+
+export default Footer;
