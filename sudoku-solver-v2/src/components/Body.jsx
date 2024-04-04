72a2adbdaadf607678e09b0384ae8dc62f039fb8
Sai Vardhan <vardhan.6405@gmail.com>
2024-04-04 22:27:39 +0530

    somehow working sudoku board (needs some refactoring)

--- sudoku-solver-v2/src/components/Body.jsx
+++ sudoku-solver-v2/src/components/Body.jsx
@@ -1,10 +1,24 @@
-import React, { Children } from "react";
+import React, { Children, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Unlock from "./icons/unlock";
 import Board from "./SudokuBoard";
+import { useEffect } from "react";
 
 function Body() {
+  const size = 9;
+
   const [searchParams, setSearchParams] = useSearchParams();
+  const [activeCell, setActiveCell] = useState([]);
+  const [currentNum, setCurrentNum] = useState(0);
+
+  useEffect(() => {
+    setCurrentNum(0);
+  }, [activeCell]);
+
+  function setNum(cellPos, num) {
+    setCurrentNum(num);
+  }
+
   return (
     <div className="px-1 py-1 sm:px-20 sm:py-10 bg-background flex flex-col ">
       <div className="h-28   flex flex-row items-center justify-center gap-32 ">
@@ -16,82 +30,71 @@ function Body() {
           Difficulty:
         </div>
         <div className="h-28 flex flex-row items-center gap-20">
-          <DiffButton OnClick={() => setSearchParams({ difficulty: "easy" })}>
+          <BigBtn OnClick={() => setSearchParams({ difficulty: "easy" })}>
             Easy
-          </DiffButton>
-          <DiffButton OnClick={() => setSearchParams({ difficulty: "medium" })}>
+          </BigBtn>
+          <BigBtn OnClick={() => setSearchParams({ difficulty: "medium" })}>
             Medium
-          </DiffButton>
-          <DiffButton OnClick={() => setSearchParams({ difficulty: "hard" })}>
+          </BigBtn>
+          <BigBtn OnClick={() => setSearchParams({ difficulty: "hard" })}>
             Hard
-          </DiffButton>
+          </BigBtn>
         </div>
       </div>
       <div className="flex justify-center p-5 gap-16">
-        <Board />
-        <div className=" w-96 py-10 flex flex-col items-center gap-16 ">
-          <div className=" grid grid-cols-3 gap-8 ">
-            <NumButton>1</NumButton>
-            <NumButton>2</NumButton>
-            <NumButton>3</NumButton>
-            <NumButton>4</NumButton>
-            <NumButton>5</NumButton>
-            <NumButton>6</NumButton>
-            <NumButton>7</NumButton>
-            <NumButton>8</NumButton>
-            <NumButton>9</NumButton>
+        <Board
+          activeCell={activeCell}
+          setActiveCell={setActiveCell}
+          num={currentNum}
+        />
+        <div className=" w-96  flex flex-col items-center gap-8 ">
+          <div className=" grid grid-cols-3 gap-6 ">
+            {[...Array(size).keys()].map((_, j) => {
+              return (
+                <NumBtn key={j} OnClick={() => setNum(activeCell, j + 1)}>
+                  {j + 1}
+                </NumBtn>
+              );
+            })}
+          </div>
+          <div className="pb-10">
+            <BigBtn OnClick={() => setActiveCell([])}>Clear</BigBtn>
           </div>
           <div className=" flex flex-row items-center justify-center gap-8">
-            <div
-              className="w-40 h-20 bg-secondary rounded-lg
-                flex justify-center items-center gap-3
-                font-inria text-4xl text-clip overflow-hidden text-text
-              hover:bg-accent hover:cursor-pointer"
-            >
-              Solve
-            </div>
-            <div
-              className="w-40 h-20 bg-secondary rounded-lg
-                flex justify-center items-center gap-3
-                font-inria text-4xl text-clip overflow-hidden text-text
-              hover:bg-accent hover:cursor-pointer"
-            >
-              Next
-            </div>
+            <BigBtn>Solve</BigBtn>
+            <BigBtn>Next</BigBtn>
           </div>
         </div>
       </div>
     </div>
   );
 }
+export default Body;
 
-function DiffButton({ OnClick, children }) {
+function BigBtn({ OnClick, children }) {
   return (
-    <>
-      <div
-        className="w-40 h-16 bg-secondary rounded-lg
-        flex justify-center items-center 
-        font-inria text-3xl text-clip overflow-hidden text-text
-        hover:bg-accent hover:cursor-pointer"
-        onClick={OnClick}
-      >
-        {children}
-      </div>
-    </>
+    <div
+      className="w-40 h-16 bg-secondary rounded-lg
+                  flex justify-center items-center 
+                  font-inria text-3xl text-clip overflow-hidden text-text select-none	
+                 hover:bg-accent hover:cursor-pointer"
+      onClick={OnClick}
+    >
+      {children}
+    </div>
   );
 }
 
-export default Body;
-
-function NumButton({ children }) {
+function NumBtn({ OnClick, children }) {
   return (
     <>
       <div
-        className="w-20 h-20
-                    rounded-[7px] bg-secondary
-                    flex justify-center items-center 
-                    font-inria text-5xl text-clip overflow-hidden text-text
+        className="size-[4.5rem] 
+        rounded-[7px] bg-secondary
+        flex justify-center items-center 
+        font-inria text-4xl text-clip overflow-hidden text-text select-none	
                     hover:bg-accent hover:cursor-pointer"
+        onClick={OnClick}
       >
         {" "}
         {children}{" "}
--- sudoku-solver-v2/src/components/SudokuBoard.jsx
+++ sudoku-solver-v2/src/components/SudokuBoard.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
-function Board() {
+function Board({ activeCell, setActiveCell, num }) {
   const size = 9;
-  const [activeCell, setActiveCell] = useState([]);
+  const [currentNum, setCurrentNum] = useState(0);
+
+  useEffect(() => {
+    setCurrentNum(num);
+  }, [num]);
+  function changeActiveCell(row, col) {
+    activeCell[0] === row && activeCell[1] === col
+      ? setActiveCell([])
+      : setActiveCell([row, col]);
+    setCurrentNum(0);
+  }
 
   return (
     <>
       <div
         className="bg-darkbg  border-2 border-text text-text 
-                      size-min flex flex-row"
+                      size-min flex flex-col"
       >
         {[...Array(size).keys()].map((_, i) => {
           return (
-            <div key={i} className="flex flex-col gap-0">
+            <div key={i} className="flex flex-row gap-0">
               {[...Array(size).keys()].map((_, j) => {
                 return (
                   <Cell
-                    key={j}
+                    key={10 * i + j}
                     row={i}
                     col={j}
                     size={size}
                     activeCell={activeCell}
-                    onClick={() => {
+                    num={
                       activeCell[0] === i && activeCell[1] === j
-                        ? setActiveCell([])
-                        : setActiveCell([i, j]);
-                    }}
+                        ? currentNum
+                        : 0
+                    }
+                    onClick={() => changeActiveCell(i, j)}
                   />
                 );
               })}
@@ -37,22 +49,30 @@ function Board() {
   );
 }
 
-function Cell({ row, col, size, onClick, activeCell }) {
+function Cell({ row, col, size, onClick, activeCell, num }) {
   const subdiv = Math.floor(Math.sqrt(size));
+  const [currentNum, setCurrentNum] = useState(0);
+
+  useEffect(() => {
+    if (num !== 0) {
+      setCurrentNum(num);
+    }
+  }, [num]);
+
   return (
     <div
       className={"size-[3.5rem] border-[1px] border-text flex items-center justify-center text-3xl select-none hover:cursor-pointer"
-        .concat(" ", row % subdiv === subdiv - 1 && " border-r-2  ")
-        .concat(" ", row % subdiv === 0 && " border-l-2  ")
-        .concat(" ", col % subdiv === subdiv - 1 && " border-b-2  ")
-        .concat(" ", col % subdiv === 0 && " border-t-2  ")
+        .concat(" ", col % subdiv === subdiv - 1 && " border-r-2  ")
+        .concat(" ", col % subdiv === 0 && " border-l-2  ")
+        .concat(" ", row % subdiv === subdiv - 1 && " border-b-2  ")
+        .concat(" ", row % subdiv === 0 && " border-t-2  ")
         .concat(
           " ",
           activeCell[0] === row && activeCell[1] === col && "bg-primary"
         )}
       onClick={onClick}
     >
-      
+      {currentNum !== 0 ? currentNum : " "}
     </div>
   );
 }
